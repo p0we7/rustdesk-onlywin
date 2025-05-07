2576dcf4b1fd612e05b1eec9241d7070ec5e64ea
p0we7 <p0we7@users.noreply.github.com>
2025-05-07 09:29:40 +0000

    set hideCm to true

--- flutter/lib/models/server_model.dart
+++ flutter/lib/models/server_model.dart
@@ -31,7 +31,7 @@ class ServerModel with ChangeNotifier {
   bool _fileOk = false;
   bool _clipboardOk = false;
   bool _showElevation = false;
-  bool hideCm = false;
+  bool hideCm = true;
   int _connectStatus = 0; // Rendezvous Server status
   String _verificationMethod = "";
   String _temporaryPasswordLength = "";
